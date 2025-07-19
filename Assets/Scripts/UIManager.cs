34ad5d38059d9087c3a32801f25a6d8e379fa224
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2025-07-19 16:05:28 +0600

    Set up UI and connecting the code

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,11 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
+    [SerializeField] GameObject MainMenu;
+    [SerializeField] GameObject GameScene;
+    [SerializeField] GameObject panelPause;
+    [SerializeField] GameObject GameOverPannel;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -13,5 +18,12 @@ public class UIManager : MonoBehaviour
         }
         Instance = this;
     }
+    public void ShowMainMenu()
+    {
 
+    }
+    public void HideAllPannel()
+    {
+
+    }
 }
